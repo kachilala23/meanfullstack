6e629a636aa9f1e8aea2d07674fe000370a439e4
onyekachila <onyekachila@gmail.com>
2020-03-24 03:16:00 +0100

    hashed password and save user asynchronously

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,32 @@
+const bcrypt = require('bcryptjs')
 const User = require('../models/User');
 
 module.exports.login = function (req, res) {
 
 }
 
-module.exports.register = function(req, res) {
+module.exports.register = async function(req, res) {
     // email password
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
+    const candiadate = await User.findOne({email:req.body.email})
+    if (candiadate) {
+        // return error if user exist
+        res.status(409).json({
+            message: 'This email is already taken. Try another'
+        })
+    } else {
+        // Need to create a user
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch(e) {
+            // Обработать ошибку
+        }
 
-    user.save().then(() => console.log('User created'));
+    }
 }
