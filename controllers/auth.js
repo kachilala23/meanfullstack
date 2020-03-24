ca635fad3288a0d63831b6392320326c3c6975f5
onyekachila <onyekachila@gmail.com>
2020-03-24 02:53:45 +0100

    create a user from postman into the database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,15 @@
+const User = require('../models/User');
+
 module.exports.login = function (req, res) {
-    res.status(200).json({
-       login: {
-           email: req.body.email,
-           password: req.body.password
-       }
-    });
+
 }
 
 module.exports.register = function(req, res) {
-    res.status(200).json({
-        register: 'from register controller'
-    })
+    // email password
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save().then(() => console.log('User created'));
 }
