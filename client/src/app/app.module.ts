3cdf8819597b7e0e4f1700532a9cd6c7aafb7327
onyekachila <onyekachila@gmail.com>
2020-04-03 01:16:01 +0100

    login page done and token set in the network tab of the broeser

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
-import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
+import {AppComponent} from './app.component'
+import {LoginPageComponent} from './login-page/login-page.component'
+import {AppRoutingModule} from './app-routing.module'
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component'
+import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component'
+import {RegisterPageComponent} from './register-page/register-page.component'
+
+
 
 @NgModule({
   declarations: [
@@ -18,9 +22,12 @@ import { RegisterPageComponent } from './register-page/register-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core'
+import {FormControl, FormGroup, Validators} from '@angular/forms'
+import {AuthService} from '../shared/services/auth.service'
+import {Subscription} from 'rxjs'
+import {ActivatedRoute, Params, Router} from '@angular/router'
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  form: FormGroup
+  aSub: Subscription
+
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+        // Now you can log in using your data
+      } else if (params['accessDenied']) {
+        // To get started, log in to the system
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.aSub) {
+      this.aSub.unsubscribe()
+    }
+  }
+
+  onSubmit() {
+    this.form.disable()
+
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      () => this.router.navigate(['/overview']),
+      error => {
+        console.warn(error)
+        this.form.enable()
+      }
+    )
   }
 
 }
