9838103e3d38830e4a4a2c9414dad5b3e2007225
onyekachila <onyekachila@gmail.com>
2020-04-08 05:29:08 +0100

    creae and edit category with imgage upload

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core'
 import {ActivatedRoute, Params} from '@angular/router'
 import {FormControl, FormGroup, Validators} from '@angular/forms'
 import {CategoriesService} from '../../shared/services/categories.service'
 import {switchMap} from 'rxjs/operators'
 import {of} from 'rxjs'
 import {MaterialService} from '../../shared/classes/material.service'
+import {Category} from '../../shared/interfaces'
 
 @Component({
   selector: 'app-categories-form',
@@ -13,8 +14,12 @@ import {MaterialService} from '../../shared/classes/material.service'
 })
 export class CategoriesFormComponent implements OnInit {
 
+  @ViewChild('input') inputRef: ElementRef;
   form: FormGroup;
+  image: File;
+  imagePreview = '';
   isNew = true;
+  category: Category;
 
   constructor(private route: ActivatedRoute,
               private categoriesService: CategoriesService) {
@@ -23,7 +28,7 @@ export class CategoriesFormComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       name: new FormControl(null, Validators.required)
-    });
+    })
 
     this.form.disable();
 
@@ -41,11 +46,13 @@ export class CategoriesFormComponent implements OnInit {
         )
       )
       .subscribe(
-        category => {
+        (category: Category) => {
           if (category) {
+            this.category = category;
             this.form.patchValue({
               name: category.name
-            })
+            });
+            this.imagePreview = category.imageSrc;
             MaterialService.updateTextInputs()
           }
 
@@ -55,8 +62,44 @@ export class CategoriesFormComponent implements OnInit {
       )
   }
 
-  onSubmit() {
+  triggerClick() {
+    this.inputRef.nativeElement.click()
+  }
 
+  onFileUpload(event: any) {
+    const file = event.target.files[0];
+    this.image = file;
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.imagePreview = <string>reader.result
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  onSubmit() {
+    let obs$;
+    this.form.disable();
+
+    if (this.isNew) {
+      obs$ = this.categoriesService.create(this.form.value.name, this.image)
+    } else {
+      obs$ = this.categoriesService.update(this.category._id, this.form.value.name, this.image)
+    }
+
+    obs$.subscribe(
+      category => {
+        this.category = category
+        MaterialService.toast('Изменения сохранены.');
+        this.form.enable()
+      },
+      error => {
+        MaterialService.toast(error.error.message);
+        this.form.enable()
+      }
+    )
   }
 
 }
